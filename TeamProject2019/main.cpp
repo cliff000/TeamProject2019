4abe811b653825146281886daa97bff13130c0f9
agdf_ydy <hakka.ydy.sub@gmail.com>
2019-03-12 14:20:32 +0900

    no message

--- TeamProject2019/main.cpp
+++ TeamProject2019/main.cpp
@@ -4,14 +4,7 @@
 #include "stage.h"
 //debug
 
-<<<<<<< HEAD
-//int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-//	DxLib_Init();	// DXライブラリ初期化処理
-//	WaitKey();	// キー入力待ち
-//	DxLib_End();	// DXライブラリ終了処理
-//	return 0;
-//}
-=======
+
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
 
@@ -41,4 +34,3 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	DxLib_End(); // DXライブラリ終了処理
 	return 0;
 }
->>>>>>> master
